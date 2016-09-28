6b9a60b31913a0e2a2bcf8d01e0c5ce450b19c0a
Alex Dacre <alex@alexdacre.com>
2016-09-28 15:41:04 +1000

    Setting up the react server

---
+++ server.babel.js
@@ -0,0 +1,11 @@
+/**
+ * Created by alex on 28/09/2016.
+ */
+
+import express from 'express';
+
+const app = express();
+
+app.use('/', express.static('public'));
+
+app.listen(process.env.PORT || 3000);
